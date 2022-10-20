36869085157be12fad713ae94dd36f167b9eac06
Rubens Galarraga <rgalarraga121@gmail.com>
2022-10-20 06:08:54 -0300

    configuraciones de yargs

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,24 +1,37 @@
-const  { crearArchivo } =  require('./helpers/multiplicar')
-const argv = require('yargs').argv;
-
+const { options } = require("yargs");
+const { crearArchivo } = require("./helpers/multiplicar");
+const argv = require("yargs")
+.option("b", {
+  alias: "base",
+  type: "number",
+  demandOption: true,
+})
+.option("l", {
+    alias: "listar",
+    type: "boolean",
+    demandOption: true,
+    default: false
+  })
+.check (( argv, options) =>{
+    if (isNaN( argv.b)) {
+       throw 'La base tiene que ser un numero'
+    }
+    return true
+})
+.argv;
 
 console.clear();
-
-
-
-    
 
 // const [ , , arg3 = 'base=5'] = process.argv;
 // const [ , base = 5] = arg3.split('=')
 
-console.log(process.argv);
+// console.log(process.argv);
 console.log(argv);
 
-console.log('base: yargs', argv.base)
+// console.log("base: yargs", argv.b);
 
 // const base = 7;
 
-// crearArchivo( base )
-// .then ( nombreArchivo => console.log(nombreArchivo, 'creado'))
-// .catch( err => console.log(err) )
-
+crearArchivo( argv.b, argv.l )
+.then ( nombreArchivo => console.log(nombreArchivo, 'creado'))
+.catch( err => console.log(err) )
